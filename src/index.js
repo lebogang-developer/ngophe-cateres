5e4c49f7e452587547feb99b8e4279a27048c241
Lebogang Sekaleli <lebogangsekaleli@gmail.com>
2024-10-30 12:14:49 +0200

    Hero section

--- src/index.js
+++ src/index.js
@@ -2,3 +2,22 @@
 document.getElementById('menu-btn').onclick = function () {
   document.getElementById('mobile-menu').classList.toggle('hidden');
 };
+
+ // Image Slider
+ const sliderImages = [
+    './assets/images/setup-5.jpg',
+    './assets/images/party-1.jpg',
+    './assets/images/setup-3.jpg',
+    './assets/images/wedding-2.jpg'
+  ];
+  let currentImageIndex = 0;
+  const slider = document.getElementById('slider');
+
+  function changeSlide() {
+    slider.style.backgroundImage = `url(${sliderImages[currentImageIndex]})`;
+    currentImageIndex = (currentImageIndex + 1) % sliderImages.length;
+  }
+
+  // Initialize slider
+  changeSlide();
+  setInterval(changeSlide, 3000); // Change image every 3 seconds
